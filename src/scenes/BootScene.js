a33fcffc99b088dd79c33aee0f34d658bcad442e
TonCrimeGame-dotcom <yagizozguney@gmail.com>
2026-03-06 16:28:05 +0300

    Update BootScene.js

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -7,10 +7,8 @@ export class BootScene {
 
   async onEnter() {
     await this.assets.loadImages([
-      // Intro / yapımcı logosu
       { key: "tata", src: "./src/assets/tata.png" },
 
-      // Genel / carousel
       { key: "weapons_bg", src: "./src/assets/weapons.png" },
       { key: "missions", src: "./src/assets/missions.jpg" },
       { key: "pvp", src: "./src/assets/pvp.jpg" },
@@ -19,14 +17,11 @@ export class BootScene {
       { key: "coffeeshop", src: "./src/assets/coffeeshop.jpg" },
       { key: "xxx", src: "./src/assets/xxx.jpg" },
 
-      // Ticaret kartı + background
       { key: "blackmarket", src: "./src/assets/BlackMarket.png" },
       { key: "blackmarket_bg", src: "./src/assets/BlackMarket.png" },
 
-      // xxx background
       { key: "xxx_bg", src: "./src/assets/xxx.jpg" },
 
-      // CoffeeShop
       { key: "coffeeshop_bg", src: "./src/assets/coffeeshop.png" },
       { key: "coffeeshop_book", src: "./src/assets/coffeeshop_book.png" },
       { key: "coffeeshop_menu", src: "./src/assets/coffeeshop_menu.png" }
@@ -42,6 +37,7 @@ export class BootScene {
     ctx.fillStyle = "#ffffff";
     ctx.font = "20px system-ui";
     ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
     ctx.fillText(this.i18n.t("loading"), w / 2, h / 2);
   }
 }
