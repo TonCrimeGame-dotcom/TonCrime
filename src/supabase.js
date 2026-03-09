00f8126aa8c5d40399de15d025be040c9f8453c5
TonCrimeGame-dotcom <yagizozguney@gmail.com>
2026-03-09 14:35:09 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -4,5 +4,3 @@ const SUPABASE_URL = "https://ubcyamjoektbbxbrjtyy.supabase.co";
 const SUPABASE_KEY = "sb_publishable_t--0L9Neb58SKtiED8K7gA_2w1gtC37";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-console.log("SUPABASE_URL =", SUPABASE_URL);
-console.log("SUPABASE_KEY_START =", SUPABASE_KEY.slice(0, 20));
