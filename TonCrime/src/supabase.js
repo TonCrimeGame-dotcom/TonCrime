10d1daa10a86d8afe23f065c677f3f25b39e55c4
TonCrimeGame-dotcom <yagizozguney@gmail.com>
2026-03-20 23:45:34 +0300

    Delete TonCrime directory

--- TonCrime/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-const SUPABASE_URL = "https://ubcyamjoektbbxbrjtyy.supabase.co";
-const SUPABASE_KEY = "sb_publishable_t--0L9Neb58SKtiED8K7gA_2w1gtC37";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
