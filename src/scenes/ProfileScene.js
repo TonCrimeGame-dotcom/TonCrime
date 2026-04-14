353442deb551d9856e26a5b4b14ff82dfdf8ee34
TonCrimeGame-dotcom <yagizozguney@gmail.com>
2026-04-14 15:32:31 +0300

    Update ProfileScene.js

--- src/scenes/ProfileScene.js
+++ src/scenes/ProfileScene.js
@@ -1,4 +1,4 @@
-import { openExternalWallet, WALLET_APP_URL } from "../walletBridge.js?v=20260413-wallet-1";
+import { openExternalWallet, WALLET_APP_URL } from "../walletBridge.js?v=20260414-stars-1";
 
 function clamp(n, a, b) {
   return Math.max(a, Math.min(b, n));
