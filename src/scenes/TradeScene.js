5c0aa204a6c6e0fdb110258535c6c1c1ea7638cc
TonCrimeGame-dotcom <yagizozguney@gmail.com>
2026-04-14 15:42:04 +0300

    Update TradeScene.js

--- src/scenes/TradeScene.js
+++ src/scenes/TradeScene.js
@@ -1,7 +1,7 @@
 import { supabase } from "../supabase.js";
 
 import { fetchBackendJson } from "../supabase.js?v=20260408-3";
-import { openExternalWallet } from "../walletBridge.js?v=20260414-stars-1";
+import { openExternalWallet } from "../walletBridge.js?v=20260414-stars-2";
 import {
   getBusinessCatalog,
   getBusinessDef,
